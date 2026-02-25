a04ecdec2c9fa587be15ba21663a97397bb5921f
tshabi16 <tshepangrethabiletshabalala@gmail.com>
2026-02-26 00:18:55 +0200

    Change image class in Card component
    Updated image class for better styling.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,9 +6,8 @@ function Card({ id, img, title, star, reviews, prevPrice, newPrice }) {
   const { addToCart, cartItems } = useContext(ShopContext);
 
   const cartItemCount = cartItems[id];
-  return (
-<section className="bg-white/5 backdrop-blur-md border border-white/10 rounded-2xl p-5 m-4 transition-all duration-300 hover:scale-105 hover:bg-white/10 hover:shadow-[0_0_20px_rgba(255,255,255,0.05)]">
-      <img src={img} alt={title} className="card-img" />
+  return ( <section className="bg-white/5 backdrop-blur-md border border-white/10 rounded-2xl p-5 m-4 transition-all duration-300 hover:scale-105 hover:bg-white/10 hover:shadow-[0_0_20px_rgba(255,255,255,0.05)]">
+      <img src={img} alt={title} className="w-full h-48 object-contain p-4" />
       <div className="card-details">
         <h4 className="card-title">{title}</h4>
         <section className="card-reviews">
